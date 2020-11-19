4887407beac12b34c4e9da5fdb97f2879b361e55
好帥 我 <g0950128@yahoo.com.tw>
2020-11-19 16:46:12 +0800

    加入專案檔案。

---
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "請輸入姓名";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 192);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(333, 246);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 65);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "伐木";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(125, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 65);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "採礦";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(238, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 65);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "打獵";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 40);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(333, 75);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(351, 12);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(197, 426);
+            this.richTextBox3.TabIndex = 7;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(554, 192);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(234, 246);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "時代演變";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(635, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+using WindowsFormsApp7.Properties;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        int iron,food,wood= 0;//設定變數
+        int namedo = 0;//用來判定是否有輸入名稱
+        int level = 0;//時代變數
+        List<Image> list = new List<Image>();
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox3.AppendText("黑暗時代：100份木頭，100份食物\n");
+            richTextBox3.AppendText("封建時代：500份木頭，300份食物，100份礦物\n");
+            richTextBox3.AppendText("城堡時代：1000份木頭，500份食物，500份礦物\n");
+            richTextBox3.AppendText("帝王時代：5000份木頭，2000份食物，1500份礦物\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Name = textBox1.Text;
+            richTextBox1.AppendText("歡迎【"+textBox1.Text+"】來到，小遊戲\n");
+            richTextBox2.AppendText("名稱：" + Name + "，");
+            richTextBox2.AppendText("木頭：" + wood+"，");
+            richTextBox2.AppendText("，礦物：" + iron+ "，");
+            richTextBox2.AppendText("，食物：" + food+ "\n");//輸入名稱後，顯示所有基本材料
+            namedo +=1;//如果有輸入名稱namedo+1，下方if判定namedo不為零
+         
+            if (namedo >= 2)
+            {
+                MessageBox.Show($"請勿重複輸入名稱", "提示", MessageBoxButtons.OK);
+                richTextBox2.Clear();
+                richTextBox1.Clear();
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int things = new Random().Next(5);//每次點選打獵，伐木，採礦隨機取得的數值
+            richTextBox2.AppendText("名稱：" + Name + "，");
+            if (namedo > 0 && namedo < 2)
+            {
+                richTextBox2.Clear();
+                wood += things;
+                richTextBox2.AppendText("木頭：" + wood);
+                richTextBox2.AppendText("，礦物：" + iron);
+                richTextBox2.AppendText("，食物：" + food);
+                richTextBox1.AppendText("拿到了" + things + "個木頭\n");
+            }
+            else
+            {
+                MessageBox.Show($"請輸入名稱", "提示", MessageBoxButtons.OK);
+                richTextBox2.Clear();
+                richTextBox1.Clear();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (level == 0)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    wood--;
+                    food--;
+                }
+                richTextBox2.Clear();
+                richTextBox2.AppendText("木頭：" + wood);
+                richTextBox2.AppendText("，礦物：" + iron);
+                richTextBox2.AppendText("，食物：" + food);
+                richTextBox1.AppendText("付出了100個木頭跟食物，演變至黑暗時代\n");
+                level++;
+                pictureBox1.Image= list[0];
+            } else if (level==1)
+            {
+                wood = wood - 500;
+                food = food - 300;
+                iron = iron - 100;
+                richTextBox2.Clear();
+                richTextBox2.AppendText("木頭：" + wood);
+                richTextBox2.AppendText("，礦物：" + iron);
+                richTextBox2.AppendText("，食物：" + food);
+                richTextBox1.AppendText("付出了500份木頭300份食物100份礦物，演變至封建時代\n");
+                level++;
+                pictureBox1.Image = list[1];
+            }
+            else if (level == 2)
+            {
+                wood = wood - 1000;
+                food = food - 500;
+                iron = iron - 500;
+                richTextBox2.Clear();
+                richTextBox2.AppendText("木頭：" + wood);
+                richTextBox2.AppendText("，礦物：" + iron);
+                richTextBox2.AppendText("，食物：" + food);
+                richTextBox1.AppendText("付出了1000份木頭500份食物500份礦物，演變至城堡時代\n");
+                level++;
+                pictureBox1.Image = list[2];
+            }
+            else if (level == 3)
+            {
+                wood = wood - 5000;
+                food = food - 2000;
+                iron = iron - 1500;
+                richTextBox2.Clear();
+                richTextBox2.AppendText("木頭：" + wood);
+                richTextBox2.AppendText("，礦物：" + iron);
+                richTextBox2.AppendText("，食物：" + food);
+                richTextBox1.AppendText("付出了5000份木頭2000份食物1500份礦物，演變至帝王時代\n");
+                pictureBox1.Image = list[3];
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list.Add(Resources._001);
+            list.Add(Resources._002);
+            list.Add(Resources._003);
+            list.Add(Resources._004);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int things = new Random().Next(5);//每次點選打獵，伐木，採礦隨機取得的數值
+            richTextBox2.AppendText("名稱：" + Name + "，");
+            if (namedo > 0 && namedo < 2)
+            {
+                richTextBox2.Clear();
+                iron += things;
+                richTextBox2.AppendText("木頭：" + wood);
+                richTextBox2.AppendText("，礦物：" + iron);
+                richTextBox2.AppendText("，食物：" + food);
+                richTextBox1.AppendText("拿到了" + things + "個礦物\n");
+            }
+            else
+            {
+                MessageBox.Show($"請輸入名稱", "提示", MessageBoxButtons.OK);
+                richTextBox2.Clear();
+                richTextBox1.Clear();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int things = new Random().Next(5);//每次點選打獵，伐木，採礦隨機取得的數值
+            richTextBox2.AppendText("名稱：" + Name + "，");
+            if (namedo > 0 && namedo < 2)
+            {
+                richTextBox2.Clear();
+                food += things;
+                richTextBox2.AppendText("木頭：" + wood);
+                richTextBox2.AppendText("，礦物：" + iron);
+                richTextBox2.AppendText("，食物：" + food);
+                richTextBox1.AppendText("拿到了"+things+ "個食物\n");
+            }
+            else
+            {
+                MessageBox.Show($"請輸入名稱", "提示", MessageBoxButtons.OK);
+                richTextBox2.Clear();
+                richTextBox1.Clear();
+            }
+        }
+    }
+}
